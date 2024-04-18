a884ddb2ff1209df2595b7b6a57a266ba0212dc5
RealThomasCat <yashveer.rtc@gmail.com>
2024-04-18 11:38:17 +0530

    Fixed UI

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-function Logo({width = '100px'}) {
+function Logo({ width = "100px" }) {
   return (
-    <div>Logo</div>
-  )
+    <div className=" text-blue-900 font-bold text-lg">
+      Blog<span className=" text-orange-600">Bud</span>
+    </div>
+  );
 }
 
-export default Logo
+export default Logo;
--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import appwriteService from "../appwrite/config"
-import {Link} from 'react-router-dom'
+import React from "react";
+import appwriteService from "../appwrite/config";
+import { Link } from "react-router-dom";
 
-function PostCard({$id, title, featuredImage}) {
-    
+function PostCard({ $id, title, featuredImage }) {
   return (
     <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-100 rounded-xl p-4'>
-            <div className='w-full justify-center mb-4'>
-                <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
-                className='rounded-xl' />
-
-            </div>
-            <h2
-            className='text-xl font-bold'
-            >{title}</h2>
+      <div className="w-full bg-gray-100 rounded-xl p-4 shadow-md">
+        <div className="w-full justify-center mb-4">
+          <img
+            src={appwriteService.getFilePreview(featuredImage)}
+            alt={title}
+            className="rounded-xl aspect-square object-cover"
+          />
         </div>
+        <h2 className="text-xl font-medium text-left text-ellipsis line-clamp-1">
+          {title}
+        </h2>
+      </div>
     </Link>
-  )
+  );
 }
 
-
-export default PostCard
+export default PostCard;
